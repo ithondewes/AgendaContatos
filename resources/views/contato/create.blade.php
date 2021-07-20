58b6675cc883cd0d20489e58b5a25460677af5c3
ithondewes <ithondewes1@gmail.com>
2021-07-20 20:22:14 -0300

    views

--- resources/views/contato/create.blade.php
+++ resources/views/contato/create.blade.php
@@ -5,7 +5,9 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Contato</div>
+                <div class="card-header">Contato
+                <a href="{{ url('contatos/add') }}" class="btn btn-primary btn-sm float-right">Detalhes Contato</a>
+                </div>
                 <form action="{{ url('contatos') }}" method="post" enctype="multipart/form-data">
                     <div class="card-body">
                         {{ csrf_field() }}
@@ -19,22 +21,6 @@
                             <div class="invalid-feedback">{{ $errors->first('nome') }}</div>
                         </div>
                         <div class="form-group">
-                            <label for="nome">Telefone(s)</label><br />
-                            <a class="btn btn-primary" href="javascript:void(0)" id="addInput">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                            Adicionar Campo
-                            </a>
-                            <br/>
-                        <div id="dynamicDiv">
-                                <p>
-                                <input type="text" required class="form-control{{$errors->has('telefone') ? ' is-invalid':''}}" value="{{ old('telefone') }}" id="telefone" name="telefone"/>
-                            <a class="btn btn-danger" href="javascript:void(0)" id="remInput">
-                                <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-                                Remover Campo
-                            </a>
-		                        </p>
-                        </div>
-                        <div class="form-group">
                             <label for="email">E-mail</label>
                             <input type="email" class="form-control{{$errors->has('email') ? ' is-invalid':''}}" value="{{ old('email') }}" id="email" name="email" placeholder="email@provedor.com.br">
                             <div class="invalid-feedback">{{ $errors->first('email') }}</div>
@@ -45,44 +31,14 @@
                             <div class="invalid-feedback">{{ $errors->first('data_nascimento') }}</div>
                         </div>
                         <div class="form-group">
-                            <label for="nome">Endereco(s)</label><br />
-                            <a class="btn btn-primary" href="javascript:void(0)" id="addInput2">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                            Adicionar Campo
-                            </a>
-                            <br/>
-                        <div id="dynamicDiv2">
-                                <p>
-                            <input type="text" required class="form-control{{$errors->has('endereco') ? ' is-invalid':''}}" value="" id="endereco" name="endereco"/>
-                            <form method="get" action=".">
-                                <label for="cep">Cep</label>
-                                <input type="text" required class="form-control{{$errors->has('cep') ? ' is-invalid':''}}" value="" id="cep" name="cep" size="10" maxlength="9"
-                                    onblur="pesquisacep(this.value);" />
-                                <div class="invalid-feedback">{{ $errors->first('cep') }}</div>    
-                                <label for="rua">Rua</label>
-                                <input type="text" required class="form-control{{$errors->has('rua') ? ' is-invalid':''}}" value="" id="rua" name="rua">
-                                <div class="invalid-feedback">{{ $errors->first('rua') }}</div>
-                                <label for="numero">Número</label>
-                                <input type="text" required class="form-control{{$errors->has('numero') ? ' is-invalid':''}}" value="" id="numero" name="numero">
-                                <div class="invalid-feedback">{{ $errors->first('numero') }}</div>
-                                <label for="complemento">Complemento</label>
-                                <input type="text" required class="form-control{{$errors->has('complemento') ? ' is-invalid':''}}" value="" id="complemento" name="complemento">
-                                <div class="invalid-feedback">{{ $errors->first('complemento') }}</div>
-                                <label for="bairro">Bairro</label>
-                                <input type="text" required class="form-control{{$errors->has('bairro') ? ' is-invalid':''}}" value="" id="bairro" name="bairro">
-                                <div class="invalid-feedback">{{ $errors->first('bairro') }}</div>
-                                <label for="cidade">Cidade</label>
-                                <input type="text" required class="form-control{{$errors->has('cidade') ? ' is-invalid':''}}" value="" id="cidade" name="cidade">
-                                <div class="invalid-feedback">{{ $errors->first('cidade') }}</div>
-                                <label for="estado">Estado</label>
-                                <input type="text" required class="form-control{{$errors->has('uf') ? ' is-invalid':''}}" value="" id="uf" name="uf">
-                                <div class="invalid-feedback">{{ $errors->first('uf') }}</div>
-                        </form>
-                        <a class="btn btn-danger" href="javascript:void(0)" id="remInput2">
-                                <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-                                Remover Campo
-                            </a>
-		                        </p>
+                            <label for="grupo_id">Grupo</label>
+                            <select class="form-control" name="grupo_id" id="grupo_id" onchange="this.form.submit()">
+                                @foreach ($grupos as $grupo)
+                                    <option value="{{ $grupo->id }}" }} >
+                                        {{ $grupo->grupo }}
+                                    </option>
+                                @endforeach
+                            </select>
                         </div>
                         <div class="form-group">
                             <label for="avatar">Imagem</label>
